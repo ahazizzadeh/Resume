a3df1469b7dd04b82d5190c0a9e1b393a3f4a062
ahazizzadeh <amirhossein_azizzadeh@yahoo.com>
2020-05-18 18:45:14 +0430

    Updates

---
+++ precache-manifest.75e0f8449aad146d49ef1f42d7dfbd55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2938c5b895a9b5d052097ee47942a461",
+    "url": "/Resume/index.html"
+  },
+  {
+    "revision": "8ac0acc0dc30dc6aca93",
+    "url": "/Resume/static/css/main.d89e8dbb.chunk.css"
+  },
+  {
+    "revision": "44cf979fddca24d81755",
+    "url": "/Resume/static/js/2.3987d9f3.chunk.js"
+  },
+  {
+    "revision": "e4d0b7b6ee221c9758b548593a6bc2ac",
+    "url": "/Resume/static/js/2.3987d9f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ac0acc0dc30dc6aca93",
+    "url": "/Resume/static/js/main.84cd0ca3.chunk.js"
+  },
+  {
+    "revision": "638c13ff20990b2ac9ce",
+    "url": "/Resume/static/js/runtime-main.66da5993.js"
+  }
+]);
